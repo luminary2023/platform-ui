f9b6328fcc7a4d55ee9e1cade5e05e4d624dc987
kofoworola01 <adenekankofoworola@gmail.com>
2023-07-23 19:59:55 +0100

    fix failing check

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,21 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { loginRequest } from '../../api/login';
-import { cookies } from 'next/headers'
 
 type Data = {
   name: string
 }
 
-export default async function longinhandler(
-  data
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
 ) {
-    
-const a = await loginRequest(data)
-cookies().set('token-test', a.token)
-
-console.log(a, '<<<<>>>>>>>')
-return a;
+  res.status(200).json({ name: 'John Doe' })
 }
-
-
