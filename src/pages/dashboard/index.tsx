a908e0aa55e286f4052a37483ec0d33eb59c6cb4
Samson Mesioye <jazzygee36gmail.com>
2024-02-28 11:30:39 +0100

    dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -94,245 +94,244 @@ const Index = () => {
       title="Dashboard"
       subtitle="An overview of your assets "
     >
-      {loading ? (
+      {/* {loading ? (
         <Loading />
-      ) : (
-        <>
-          {profile ? (
-            <>
-              <div className={styles.completeProfile}>
-                <h1>Complete your profile</h1>
+      ) : ( */}
+      <>
+        {profile ? (
+          <>
+            <div className={styles.completeProfile}>
+              <h1>Complete your profile</h1>
 
-                <div className={styles.profile}>
-                  <div>
-                    <p>
-                      Finish setting up your Wallet To buy, sell <br /> and
-                      recieve cryto without limits.
-                    </p>
-                  </div>
+              <div className={styles.profile}>
+                <div>
+                  <p>
+                    Finish setting up your Wallet To buy, sell <br /> and
+                    recieve cryto without limits.
+                  </p>
+                </div>
 
-                  <Box
+                <Box
+                  sx={{
+                    position: "relative",
+                    display: { lg: "flex", md: "flex", xs: "none" },
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
                     sx={{
-                      position: "relative",
-                      display: { lg: "flex", md: "flex", xs: "none" },
-                      alignItems: "center",
+                      marginLeft: "107px",
+                      position: "absolute",
+                      fontFamily: "Satoshi Bold",
+                      fontSize: "16px",
+                      fontWeight: "bold",
                     }}
                   >
-                    <Typography
-                      sx={{
-                        marginLeft: "107px",
-                        position: "absolute",
-                        fontFamily: "Satoshi Bold",
-                        fontSize: "16px",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      {progress}/3
-                    </Typography>
-                    <CircularProgress
-                      variant="determinate"
-                      value={100}
-                      thickness={8}
-                      size={80}
-                      sx={{
-                        color: progress === 3 ? "green" : "#F2F4FC",
+                    {progress}/3
+                  </Typography>
+                  <CircularProgress
+                    variant="determinate"
+                    value={100}
+                    thickness={8}
+                    size={80}
+                    sx={{
+                      color: progress === 3 ? "green" : "#F2F4FC",
 
-                        width: {
-                          xs: "20px",
-                          sm: "20px",
-                          lg: "100px",
-                          xl: "100px",
-                        },
-                        height: {
-                          xs: "20px",
-                          sm: "20px",
-                          lg: "100px",
-                          xl: "100px",
-                        },
-                        position: "relative",
-                        top: "50%",
-                        left: "50%",
-                        marginTop: "0px",
-                        marginRight: "80px",
-                      }}
-                    />
-                  </Box>
-                </div>
+                      width: {
+                        xs: "20px",
+                        sm: "20px",
+                        lg: "100px",
+                        xl: "100px",
+                      },
+                      height: {
+                        xs: "20px",
+                        sm: "20px",
+                        lg: "100px",
+                        xl: "100px",
+                      },
+                      position: "relative",
+                      top: "50%",
+                      left: "50%",
+                      marginTop: "0px",
+                      marginRight: "80px",
+                    }}
+                  />
+                </Box>
               </div>
+            </div>
 
-              <div className={styles.profileSecurity}>
-                <Image
-                  src={KYC}
-                  alt="secure"
-                  className={styles.profileSecurityImg}
+            <div className={styles.profileSecurity}>
+              <Image
+                src={KYC}
+                alt="secure"
+                className={styles.profileSecurityImg}
+              />
+              <div>
+                <h3>KYC Verification</h3>
+
+                <p>
+                  Submit your Identification documents and increase your wallet
+                  limits.
+                </p>
+              </div>
+            </div>
+            <div
+              className={styles.profileSecurity}
+              onClick={() => {
+                if (profileData.isTransactionPinSet === 0)
+                  setOpenTransactionPin(true);
+              }}
+              style={{ cursor: "pointer" }}
+            >
+              {/* <Image
+              src={KYC}
+              alt="secure"
+              className={styles.profileSecurityImg}
+            /> */}
+              <Box
+                sx={{
+                  width: { lg: "40px", md: "6%", sm: "55px", xs: "55px" },
+                  height: "40px",
+                  borderRadius: "100%",
+                  background: "#081630",
+                  // position: "relative",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginRight: "7%",
+                }}
+              >
+                <ReceiptLongIcon
+                  sx={{
+                    color: "#fff",
+                    width: "70%",
+                  }}
                 />
-                <div>
-                  <h3>KYC Verification</h3>
+              </Box>
+              <div>
+                <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+                  <h3>Create Transaction Pin</h3>
 
-                  <p>
-                    Submit your Identification documents and increase your
-                    wallet limits.
-                  </p>
-                </div>
+                  {profileData?.isTransactionPinSet === 1 && (
+                    <CheckIcon sx={{ color: "green" }} />
+                  )}
+                </Box>
+
+                <p>To be able to transact create transaction pin</p>
               </div>
-              <div
-                className={styles.profileSecurity}
-                onClick={() => {
-                  if (profileData.isTransactionPinSet === 0)
-                    setOpenTransactionPin(true);
-                }}
-                style={{ cursor: "pointer" }}
-              >
-                {/* <Image
+            </div>
+
+            <div
+              className={styles.profileSecurity}
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                if (profileData?.phoneNumber === null) setOpenPhoneModal(true);
+              }}
+            >
+              {/* <Image
               src={KYC}
               alt="secure"
               className={styles.profileSecurityImg}
             /> */}
-                <Box
-                  sx={{
-                    width: { lg: "40px", md: "6%", sm: "55px", xs: "55px" },
-                    height: "40px",
-                    borderRadius: "100%",
-                    background: "#081630",
-                    // position: "relative",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginRight: "7%",
-                  }}
-                >
-                  <ReceiptLongIcon
-                    sx={{
-                      color: "#fff",
-                      width: "70%",
-                    }}
-                  />
-                </Box>
-                <div>
-                  <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
-                    <h3>Create Transaction Pin</h3>
-
-                    {profileData?.isTransactionPinSet === 1 && (
-                      <CheckIcon sx={{ color: "green" }} />
-                    )}
-                  </Box>
-
-                  <p>To be able to transact create transaction pin</p>
-                </div>
-              </div>
-
-              <div
-                className={styles.profileSecurity}
-                style={{ cursor: "pointer" }}
-                onClick={() => {
-                  if (profileData?.phoneNumber === null)
-                    setOpenPhoneModal(true);
+              <Box
+                sx={{
+                  width: { lg: "40px", md: "40px", sm: "75px", xs: "75px" },
+                  height: "40px",
+                  borderRadius: "100%",
+                  background: "#081630",
+                  // position: "relative",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginRight: "7%",
                 }}
               >
-                {/* <Image
-              src={KYC}
-              alt="secure"
-              className={styles.profileSecurityImg}
-            /> */}
-                <Box
+                <PhoneInTalkIcon
                   sx={{
-                    width: { lg: "40px", md: "40px", sm: "75px", xs: "75px" },
-                    height: "40px",
-                    borderRadius: "100%",
-                    background: "#081630",
-                    // position: "relative",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginRight: "7%",
+                    color: "#fff",
+                    width: "70%",
                   }}
-                >
-                  <PhoneInTalkIcon
-                    sx={{
-                      color: "#fff",
-                      width: "70%",
-                    }}
-                  />
+                />
+              </Box>
+              <div>
+                <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+                  <h3>Verify Phone number</h3>{" "}
+                  {profileData?.isPhoneNumberVerified === 1 && (
+                    <CheckIcon sx={{ color: "green" }} />
+                  )}
                 </Box>
-                <div>
-                  <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
-                    <h3>Verify Phone number</h3>{" "}
-                    {profileData?.isPhoneNumberVerified === 1 && (
-                      <CheckIcon sx={{ color: "green" }} />
-                    )}
-                  </Box>
-                  <p>
-                    keep your account more secure by entering your phone number.
-                  </p>
-                </div>
+                <p>
+                  keep your account more secure by entering your phone number.
+                </p>
               </div>
-              <div
-                className={styles.profileSecurity}
-                style={{ cursor: "pointer" }}
-                onClick={handleBankDetailsModal}
-              >
-                {/* <Image
+            </div>
+            <div
+              className={styles.profileSecurity}
+              style={{ cursor: "pointer" }}
+              onClick={handleBankDetailsModal}
+            >
+              {/* <Image
               src={Bank}
               alt="secure"
               className={styles.profileSecurityImg}
             /> */}
-                <Box
-                  sx={{
-                    width: { lg: "40px", md: "40px", sm: "70px", xs: "65px" },
-                    height: "40px",
-                    borderRadius: "100%",
-                    background: "#081630",
-                    // position: "relative",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginRight: "7%",
-                  }}
-                >
-                  <AccountBalanceIcon
-                    sx={{
-                      color: "#fff",
-                      width: "70%",
-                    }}
-                  />
-                </Box>
-                <div>
-                  <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
-                    {" "}
-                    <h3>Bank Details</h3>{" "}
-                    {bankAccount?.length > 0 && (
-                      <CheckIcon sx={{ color: "green" }} />
-                    )}
-                  </Box>
-                  <p>Recieve money into your prefered bank account.</p>
-                </div>
-              </div>
-              <Button
-                color="primary"
-                variant="contained"
+              <Box
                 sx={{
-                  textTransform: "initial",
-                  width: "120px",
+                  width: { lg: "40px", md: "40px", sm: "70px", xs: "65px" },
+                  height: "40px",
+                  borderRadius: "100%",
+                  background: "#081630",
+                  // position: "relative",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginRight: "7%",
                 }}
-                onClick={() => setProfile(false)}
               >
-                Skip for now
-              </Button>
-            </>
-          ) : (
-            <Dashboard />
-          )}
-          <BankDetailsModal open={open} onClose={() => setOpen(false)} />
-          <PhoneNumberModal
-            open={openPhoneModal}
-            onClose={() => setOpenPhoneModal(false)}
-          />
-          <TransactionPinModal
-            open={openTransactionPin}
-            onClose={() => setOpenTransactionPin(false)}
-          />
-        </>
-      )}
+                <AccountBalanceIcon
+                  sx={{
+                    color: "#fff",
+                    width: "70%",
+                  }}
+                />
+              </Box>
+              <div>
+                <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+                  {" "}
+                  <h3>Bank Details</h3>{" "}
+                  {bankAccount?.length > 0 && (
+                    <CheckIcon sx={{ color: "green" }} />
+                  )}
+                </Box>
+                <p>Recieve money into your prefered bank account.</p>
+              </div>
+            </div>
+            <Button
+              color="primary"
+              variant="contained"
+              sx={{
+                textTransform: "initial",
+                width: "120px",
+              }}
+              onClick={() => setProfile(false)}
+            >
+              Skip for now
+            </Button>
+          </>
+        ) : (
+          <Dashboard />
+        )}
+        <BankDetailsModal open={open} onClose={() => setOpen(false)} />
+        <PhoneNumberModal
+          open={openPhoneModal}
+          onClose={() => setOpenPhoneModal(false)}
+        />
+        <TransactionPinModal
+          open={openTransactionPin}
+          onClose={() => setOpenTransactionPin(false)}
+        />
+      </>
+      {/* )} */}
     </DashboardContainer>
   );
 };
